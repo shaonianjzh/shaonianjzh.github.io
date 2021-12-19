26406d4ad83abacd1d3262a7926608bc6df32f1e
&少年 <76216210+shaonianjzh@users.noreply.github.com>
2021-12-19 11:48:19 +0800

    Add files via upload

---
+++ js/route_detail.js
@@ -0,0 +1,9 @@
+function changeImg(id) {
+	document.getElementById("big-img").src = document.getElementById(id).src;
+}
+
+function num() {
+	var count = document.getElementById("count").innerHTML;
+
+	document.getElementById("count").innerHTML = Number(count) + 1;
+}
