8ebeab45dbaead9af45c711987ebaf78f2dc3590
shaonian <76216210+shaonianjzh@users.noreply.github.com>
2021-12-28 14:50:07 +0800

    trip web

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function IsLogin1() {
+	if (getCookie("username").trim().length==0 && getCookie("password").trim().length==0) {
+		alert("请先登录");
+		return false;
+	} else {
+		document.getElementById("myfavorite").href = "myfavorite1.html";
+	}
+}
+
+function IsLogin2() {
+	if (getCookie("username").trim().length==0 && getCookie("password").trim().length==0) {
+		alert("请先登录");
+		return false;
+	} else {
+		document.getElementById("route").href = "route_detail.html";
+	}
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+
+	var arrays = document.cookie.split(";");
+	for (var i = 0; i < arrays.length; i++) {
+
+		var c = arrays[i].trim();
+		if (c.indexOf(name) == 0)
+			return c.substring(name.length, c.length);
+	}
+	return "";
+}
---
+++ js/login.js
@@ -0,0 +1,28 @@
+function checkForm() {
+	var username = document.getElementById("username");
+	var password = document.getElementById("password");
+	
+	if (username.value == getCookie("username") && password.value == getCookie("password")) {
+		
+		alert("登录成功");
+		return true;
+		
+	} else {
+		alert("账号或密码错误！");
+		return false;
+	}
+}
+
+function getCookie(cname) {
+	
+	var name = cname + "=";
+
+	var arrays = document.cookie.split(";");
+	for (var i = 0; i < arrays.length; i++) {
+
+		var c = arrays[i].trim();
+		if (c.indexOf(name) == 0)
+			return c.substring(name.length, c.length);
+	}
+	return "";
+}
